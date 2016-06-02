aa3e7b83315a07e1c1010c099067af65d9517779
Your Name <elfimov-val@yandex.ua>
2016-06-02 23:34:34 +0300

    push

---
+++ Demonstration/Project/js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+// ------------------------------------------------------------------------------------
+  
+/* ===== INITIALIZATION OF THE PLUGINS: ===== */
+
+/* ----- name of the plugin ----- */
+
+/* ===== CUSTOM CODE: ===== */
+  
+/* ----- side menu ----- */
+
+$('.navbar-nav-close').on('click', function(event) {
+	event.preventDefault();
+	$('.navbar-nav').toggleClass('is-visible');
+	$(this).toggleClass('is-open');
+});
+
+// -------------------------------------------------------------------------------------
+});
