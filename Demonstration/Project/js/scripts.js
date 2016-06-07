52444c1eedb4e674b339bcbc06d3cffc33ca1b45
Your Name <elfimov-val@yandex.ua>
2016-06-07 13:16:51 +0300

    new

--- Demonstration/Project/js/scripts.js
+++ Demonstration/Project/js/scripts.js
@@ -13,6 +13,38 @@ $('body').on('click', '.btn__callback', function(event) {
 
 });
 
+/* ----- flexSlider.js ----- */
+
+$('#tv-slider').flexslider({
+  animation: "fade",
+  slideshowSpeed: 4000,
+  animationSpeed: 1000,
+  initDelay: 1000,
+  controlNav: false,
+  directionNav: false
+});
+
+$('#leaders-carousel').flexslider({
+	animation: "slide",
+	controlNav: false,
+  animationLoop: true,
+  itemWidth: 240,
+  slideshow: true,
+  asNavFor: '#leaders-slider'
+});
+
+$('#leaders-slider').flexslider({
+	animation: "slide",
+	slideshowSpeed: 4000,
+  animationSpeed: 1000,
+	animationLoop: true,
+	slideshow: true,
+	controlNav: false,
+	controlsContainer: $(".custom-controls-container"),
+	customDirectionNav: $(".custom-navigation a"),
+	sync: "#leaders-carousel"
+});
+
 /* ===== CUSTOM CODE: ===== */
   
 /* ----- toggle menu ----- */
@@ -24,3 +56,30 @@ $('.navbar').on('click', '.btn-menu', function(event) {
 
 // -------------------------------------------------------------------------------------
 });
+
+/* ----- yandex map ----- */
+
+ymaps.ready(init);
+
+function init () {
+  var myMap = new ymaps.Map("map", {
+    center: [61.254623,73.425984],
+    zoom: 16
+  });
+
+  // Создаем метку с помощью вспомогательного класса.
+  var myPlacemark = new ymaps.Placemark([61.254623,73.425984], {
+    // Свойства.
+    // Содержимое иконки, балуна и хинта.
+    balloonContent: '',
+    hintContent: 'ул. 30 лет Победы, 27/2'
+  });
+ 	// иконка
+  myPlacemark.options.set('iconImageHref', '../img/balloon.png');
+  myPlacemark.options.set('iconImageOffset', [-45, -130]);
+  myPlacemark.options.set('iconImageSize', [75,123]);
+    
+  myMap.controls.add('smallZoomControl');
+  // Добавляем все метки на карту.
+  myMap.geoObjects.add(myPlacemark);
+};
