f633cb6bf1db472a30452bf8db944c246efb2a7b
Your Name <elfimov-val@yandex.ua>
2016-06-14 18:56:43 +0300

    new

---
+++ Demonstration/js/scripts.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+// ------------------------------------------------------------------------------------
+  
+/* ===== INITIALIZATION OF THE PLUGINS: ===== */
+
+/* ----- flexSlider ----- */
+
+(function() {
+ 
+  // store the slider in a local variable
+  var $window = $(window),
+      flexslider;
+ 
+  // tiny helper function to add breakpoints
+  function getGridSize() {
+    return (window.innerWidth < 480) ? 1 :
+  				 (window.innerWidth < 768) ? 2 : 4;
+  }
+ 
+  $window.load(function() {
+    $('.flexslider-1').flexslider({
+      animation: "slide",
+      itemWidth: 220.5,
+      itemMargin: (function() {
+    		return (window.innerWidth < 1024) ? 20 : 26
+  		})(),
+      controlNav: false,
+      slideshow: false,
+      prevText: "",
+      nextText: "",
+      animationLoop: true,
+      minItems: getGridSize(), // use function to pull in initial value
+      maxItems: getGridSize() // use function to pull in initial value
+    });
+  });
+
+  $window.load(function() {
+    $('.flexslider-2').flexslider({
+      animation: "slide",
+      animationLoop: true,
+      itemWidth: 220.5,
+      controlNav: false,
+      slideshow: false,
+      itemMargin: (function() {
+    		return (window.innerWidth < 1024) ? 20 : 26
+  		})(),
+      prevText: "",
+      nextText: "",
+      minItems: getGridSize(), // use function to pull in initial value
+      maxItems: getGridSize() // use function to pull in initial value
+    });
+  });
+ 
+  // check grid size on resize event
+  $window.resize(function() {
+    var gridSize = getGridSize();
+ 
+    flexslider.vars.minItems = gridSize;
+    flexslider.vars.maxItems = gridSize;
+  });
+}());
+
+/* ===== CUSTOM CODE: ===== */
+  
+/* ----- toTop ----- */
+
+$('#toTop').click( function() {
+  $('html, body').animate({ scrollTop: 0 }, 700);
+	return false;
+});
+
+/* ----- side menu ----- */
+
+$('.btn-menu').on('click', function(event) {
+  event.preventDefault();
+  $('.navbar').toggleClass('is-visible');
+  $(this).toggleClass('is-open');
+});
+
+// -------------------------------------------------------------------------------------
+});
