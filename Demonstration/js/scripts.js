666205263091cee611a10564dc47a79e35b46851
Your Name <elfimov-val@yandex.ua>
2016-06-14 20:30:21 +0300

    new

--- Demonstration/js/scripts.js
+++ Demonstration/js/scripts.js
@@ -5,60 +5,79 @@ $(document).ready(function() {
 
 /* ----- flexSlider ----- */
 
-(function() {
- 
-  // store the slider in a local variable
-  var $window = $(window),
-      flexslider;
- 
-  // tiny helper function to add breakpoints
-  function getGridSize() {
-    return (window.innerWidth < 480) ? 1 :
-  				 (window.innerWidth < 768) ? 2 : 4;
-  }
- 
-  $window.load(function() {
-    $('.flexslider-1').flexslider({
-      animation: "slide",
-      itemWidth: 220.5,
-      itemMargin: (function() {
-    		return (window.innerWidth < 1024) ? 20 : 26
-  		})(),
-      controlNav: false,
-      slideshow: false,
-      prevText: "",
-      nextText: "",
-      animationLoop: true,
-      minItems: getGridSize(), // use function to pull in initial value
-      maxItems: getGridSize() // use function to pull in initial value
-    });
+var $window = $(window);
+
+$window.resize(function(event) {
+
+  $('.flexslider-1').removeData("flexslider");
+  $('.flexslider-2').removeData("flexslider");
+
+  $('.flexslider-1').flexslider({
+    animation: "slide",
+    itemWidth: 220.5,
+    itemMargin: (function() {
+      return (window.innerWidth < 1024) ? 20 : 26
+    })(),
+    controlNav: false,
+    slideshow: false,
+    prevText: "",
+    nextText: "",
+    animationLoop: true,
+    minItems: getGridSize(), // use function to pull in initial value
+    maxItems: getGridSize() // use function to pull in initial value
   });
 
-  $window.load(function() {
-    $('.flexslider-2').flexslider({
-      animation: "slide",
-      animationLoop: true,
-      itemWidth: 220.5,
-      controlNav: false,
-      slideshow: false,
-      itemMargin: (function() {
-    		return (window.innerWidth < 1024) ? 20 : 26
-  		})(),
-      prevText: "",
-      nextText: "",
-      minItems: getGridSize(), // use function to pull in initial value
-      maxItems: getGridSize() // use function to pull in initial value
-    });
+  $('.flexslider-2').flexslider({
+    animation: "slide",
+    animationLoop: true,
+    itemWidth: 220.5,
+    controlNav: false,
+    slideshow: false,
+    itemMargin: (function() {
+      return (window.innerWidth < 1024) ? 20 : 26
+    })(),
+    prevText: "",
+    nextText: "",
+    minItems: getGridSize(), // use function to pull in initial value
+    maxItems: getGridSize() // use function to pull in initial value
   });
+});
  
-  // check grid size on resize event
-  $window.resize(function() {
-    var gridSize = getGridSize();
+  // tiny helper function to add breakpoints
+function getGridSize() {
+  return (window.innerWidth < 480) ? 1 :
+  			 (window.innerWidth < 768) ? 2 : 4;
+}
  
-    flexslider.vars.minItems = gridSize;
-    flexslider.vars.maxItems = gridSize;
-  });
-}());
+$('.flexslider-1').flexslider({
+  animation: "slide",
+  itemWidth: 220.5,
+  itemMargin: (function() {
+		return (window.innerWidth < 1024) ? 20 : 26
+	})(),
+  controlNav: false,
+  slideshow: false,
+  prevText: "",
+  nextText: "",
+  animationLoop: true,
+  minItems: getGridSize(), // use function to pull in initial value
+  maxItems: getGridSize() // use function to pull in initial value
+});
+
+$('.flexslider-2').flexslider({
+  animation: "slide",
+  animationLoop: true,
+  itemWidth: 220.5,
+  controlNav: false,
+  slideshow: false,
+  itemMargin: (function() {
+		return (window.innerWidth < 1024) ? 20 : 26
+	})(),
+  prevText: "",
+  nextText: "",
+  minItems: getGridSize(), // use function to pull in initial value
+  maxItems: getGridSize() // use function to pull in initial value
+});
 
 /* ===== CUSTOM CODE: ===== */
   
