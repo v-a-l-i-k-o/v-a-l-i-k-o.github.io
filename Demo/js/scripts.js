9a457e3e4bde804952660ce100d0a8e15058bbb3
v-a-l-i-k-o <elfimov-val@yandex.ua>
2016-09-13 13:05:22 +0300

    edit

--- Demo/js/scripts.js
+++ Demo/js/scripts.js
@@ -25,11 +25,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
       // enable inertial throwing
       inertia: true,
       // keep the element within the area of it's parent
-      restrict: {
-        restriction: "parent",
-        endOnly: true,
-        elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-      },
+      // restrict: {
+      //   restriction: "parent",
+      //   elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+      // },
       // enable autoScroll
       autoScroll: true,
 
