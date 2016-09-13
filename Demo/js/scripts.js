aba0b2577c1d13c82a2af1b0cc3be4bbc9617159
v-a-l-i-k-o <elfimov-val@yandex.ua>
2016-09-13 11:35:07 +0300

    edit

--- Demo/js/scripts.js
+++ Demo/js/scripts.js
@@ -27,6 +27,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
       // keep the element within the area of it's parent
       restrict: {
         restriction: "parent",
+        endOnly: true,
         elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
       },
       // enable autoScroll
