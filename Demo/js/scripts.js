819c7e3966d54f90799a8c5b113ae1433b173dda
v-a-l-i-k-o <elfimov-val@yandex.ua>
2016-10-18 20:57:24 +0300

    edit

--- Demo/js/scripts.js
+++ Demo/js/scripts.js
@@ -99,7 +99,31 @@ $(document).ready(function() {
   
 	/* ----- description of your code ----- */
 
-
-
 // -------------------------------------------------------------------------------------
 });
+
+/* ----- yandex map ----- */
+
+ymaps.ready(init);
+
+function init () {
+	var myMap = new ymaps.Map("map", {
+		center: [55.79635956893193,37.710413500000016],
+		zoom: 16
+	});
+	// Создаем метку с помощью вспомогательного класса.
+	var myPlacemark = new ymaps.Placemark([55.79635956893193,37.710413500000016], {
+		// Свойства.
+		// Содержимое иконки, балуна и хинта.
+		balloonContent: '',
+		hintContent: 'г. Москва, Площадь, ул.1-я Бухвостова 12/11 офис 211'
+	});
+	// иконка
+	myPlacemark.options.set('iconImageHref', './img/icons/balloon.png');
+	myPlacemark.options.set('iconImageOffset', [-15, -50]);
+	myPlacemark.options.set('iconImageSize', [38, 53]);
+	// Добавляем zoom.
+	myMap.controls.add('smallZoomControl');
+	// Добавляем все метки на карту.
+	myMap.geoObjects.add(myPlacemark);
+};
