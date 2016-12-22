ee0ec7285b383b9fe918b8aaab3dc7a05a9c8c90
v-a-l-i-k-o <elfimov-val@yandex.ua>
2016-12-22 12:01:55 +0200

    edit

--- Demo/js/scripts.js
+++ Demo/js/scripts.js
@@ -9,9 +9,37 @@ $(document).ready(function() {
 
 /* ===== CUSTOM CODE: ===== */
   
-	/* ----- description of your code ----- */
+	/* ----- accordion ----- */
 
+	accordionInit('.accordion');
 
+	function accordionInit(el) {
+		var closeAccordion = function() {
+			$('.accordion_title').removeClass('active');
+			$('.accordion_content').slideUp(300).removeClass('open');
+		};
+
+		$(el).on('click', '.accordion_title', function(e) {
+			if (e.target.tagName !== 'LABEL' && e.target.tagName !== 'INPUT' && e.target.tagName !== 'I') {
+				var multiple = $(this).closest(el).is('.multiple');
+				var attrHref = $(this).attr('href');
+
+				if ($(this).is('.active')) {
+
+					$(this).removeClass('active');
+					$(attrHref).slideUp(300).removeClass('open');
+
+				} else {
+					if (multiple) closeAccordion();
+
+					$(this).addClass('active');
+					$(attrHref).slideDown(300).addClass('open');
+				}
+			}
+
+			e.preventDefault();
+		});
+	};
 
 // -------------------------------------------------------------------------------------
 });
